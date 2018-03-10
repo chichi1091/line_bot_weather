639ebd17d6b141911d677a042d832af3100a0726
ChiharuTerashima <chichi.pa2.1091@gmail.com>
2018-03-10 22:53:36 +0900

    メッセージ送信

--- bot/views.py
+++ bot/views.py
@@ -38,11 +38,10 @@ class LineBotView(APIView):
 
         return Response("OK", HTTP_200_OK)
 
+    @handler.add(MessageEvent, message=TextMessage)
+    def handle_message(self, event):
+        logger.info(event)
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    logger.info(event)
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
